c9ddc60645fe86fc81ee41f2739a2f4fb0b95e9e
adarshgowdaa <adarshgowda463@gmail.com>
2023-07-02 20:59:20 +0530

    Updated HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState, useEffect, useRef } from "react";
 import "./App.css";
 import { uploadFile } from "./service/api";
+import logo from "./img/image.jpg";
+import {
+  ChakraProvider,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Divider,
+  Text,
+  Button,
+  InputRightElement,
+  Input,
+  InputGroup,
+  useToast,
+} from "@chakra-ui/react";
 
 function App() {
   const [file, setFile] = useState("");
   const [result, setResult] = useState("");
 
   const fileInputRef = useRef();
-
-  const url =
-    "https://i.pinimg.com/originals/16/46/24/1646243661201a0892cc4b1a64fcbacf.jpg";
+  const toast = useToast();
 
   useEffect(() => {
     const getImage = async () => {
@@ -29,26 +44,83 @@ function App() {
     fileInputRef.current.click();
   };
 
+  const copyHandler = () => {
+    navigator.clipboard.writeText(result);
+    toast({
+      title: "Copied!",
+      description: "Link copied to clipboard.",
+      status: "success",
+      duration: 2000,
+      isClosable: true,
+    });
+  };
+
   return (
-    <div className="container">
-      <img src={url} className="img" alt="" />
-      <div className="wrapper">
-        <h1>Simple file sharing!</h1>
-        <p>Upload and share the download link.</p>
+    <>
+      <div className="main">
+        <div className="card" >
+          <ChakraProvider>
+            <Card maxW="sm" style={{ margin: "auto" }}>
+              <CardBody>
+                <Image
+                  src={logo}
+                  alt="Green double couch with wooden legs"
+                  borderRadius="lg"
+                />
 
-        <button onClick={() => onUploadClick()}>Upload</button>
-        <input
-          type="file"
-          ref={fileInputRef}
-          style={{ display: "none" }}
-          onChange={(e) => setFile(e.target.files[0])}
-        />
-
-        <a href={result} target="_blank">
-          {result}
-        </a>
+                <Divider />
+                <CardFooter>
+                  <Button
+                    variant="solid"
+                    colorScheme="yellow"
+                    style={{ margin: "auto" }}
+                    onClick={() => onUploadClick()}
+                  >
+                    Upload
+                  </Button>
+                  <input
+                    type="file"
+                    ref={fileInputRef}
+                    style={{ display: "none" }}
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </CardFooter>
+                <Stack mt="6" spacing="3">
+                  <Heading size="md">Link is being generated...</Heading>
+                  <Text>
+                    Wait for the upload to be completed and your link will be
+                    generated below.
+                  </Text>
+                </Stack>
+              </CardBody>
+              <Divider />
+              <CardBody>
+                <div href={result} target="_blank">
+                  {result && (
+                    <InputGroup size="md">
+                      <Input pr="5.5rem" type="text" value={result} />
+                      <InputRightElement
+                        width="4.5rem"
+                        style={{ marginTop: "1.2rem", marginRight: "0.3rem" }}
+                      >
+                        <Button
+                          h="1.75rem"
+                          size="md"
+                          onClick={copyHandler}
+                          colorScheme="yellow"
+                        >
+                          Copy
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  )}
+                </div>
+              </CardBody>
+            </Card>
+          </ChakraProvider>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
